4add8e1244c2ba555e13c2b0930774822fa75ad2
Irfanwani <irfanwani347@gmail.com>
2024-01-25 18:35:13 +0530

    added some modifications

--- practical1/linearsearch.cpp
+++ practical1/linearsearch.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 int main() {
 
-int a[10] = {1, 2, 3, 4, 5, 34, 23, 12, 56, 65};
-int x = 2;
+int a[11] = {1, 2, 3, 4, 5, 34, 23, 12, 56, 65};
+int x = 2, f = 0;
 // less efficient
 for(int i = 0; i < 10; i++) {
     if (a[i] == x) {
         cout << "Index of " << x << " is: " << i << endl;
-        return 0; 
+        f = 1;
+        break;
     } 
 }
 
-cout << "Element not found" << endl;
+if(f == 0) cout << "Element not found" << endl;
 
 // more efficient way
 
-a[11] = x;
+a[10] = x;
 int i = 0;
 for(i = 0; a[i] != x; i++);
 if(i != 11) {
